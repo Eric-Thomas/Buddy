e5408071fe4e72c6d4fd0560d15889625796df65
Michael Huang <huang.2721@osu.edu>
2019-11-02 14:55:40 -0400

    Front page first edition

--- buddy.py
+++ buddy.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 import json
 
 key = 'D2ZCny0cmDMUDUbsavWda8qvTD9K3iRV'
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/directions')
 def get_directions():
     params = {'key': key, 'from': '1900 Summit Street Columbus Ohio', 'to': '1494 North High Street Columbus Ohio', 'routeType': 'pedestrian', 'narrativeType': 'html'}
     content = requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
     parsed_json = json.loads(content)
     return content
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host = '0.0.0.0', debug=True)
