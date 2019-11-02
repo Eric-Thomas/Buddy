7ca41466c645d1ced5edfb0c14267f319457eaea
Eric Thomas <bluedevils027@gmail.com>
2019-11-02 12:38:20 -0400

    getting route data

--- buddy.py
+++ buddy.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import requests
+import json
 
 key = 'D2ZCny0cmDMUDUbsavWda8qvTD9K3iRV'
 app = Flask(__name__)
 
 @app.route('/')
-def get_route():
+def get_directions():
     params = {'key': key, 'from': '1900 Summit Street Columbus Ohio', 'to': '1494 North High Street Columbus Ohio', 'routeType': 'pedestrian', 'narrativeType': 'html'}
-    return requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
+    content = requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
+    parsed_json = json.loads(content)
+    return content
 
 
 if __name__ == '__main__':
