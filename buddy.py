e5c32581bc71627e3c93490be5c1094a1285279d
Eric Thomas <bluedevils027@gmail.com>
2019-11-02 13:56:15 -0400

    returns narrative, distance, and startpoint

--- buddy.py
+++ buddy.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
 import pprint
@@ -6,18 +6,28 @@ import pprint
 key = 'D2ZCny0cmDMUDUbsavWda8qvTD9K3iRV'
 app = Flask(__name__)
 
-@app.route('/directions')
+@app.route('/directions', methods=['GET'])
 def get_directions():
-    params = {'key': key, 'from': '1900 Summit Street Columbus Ohio', 'to': '1494 North High Street Columbus Ohio', 'routeType': 'pedestrian', 'narrativeType': 'html'}
+    # start = request.args.get('from')
+    # end = request.args.get('to')
+    start = '1900 Summit Street Columbus Ohio'
+    end = '1494 North High Street Columbus Ohio'
+    params = {'key': key, 'from': start, 'to': end, 'routeType': 'pedestrian', 'narrativeType': 'html'}
     content = requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
     parsed_json = json.loads(content)
     pp = pprint.PrettyPrinter()
-    directions = {'directions': ''}
+    pp.pprint(parsed_json)
+    resp = {'directions': [],}
     for leg in parsed_json['route']['legs']:
         for maneuver in leg['maneuvers']:
-            directions['directions'] += maneuver['narrative']
-    return json.dumps(directions)
+            data = {'narrative': '', 'distance': '', 'startPoint': ''}
+            data['narrative'] = maneuver['narrative']
+            data['distance'] = maneuver['distance']
+            data['startPoint'] = maneuver['startPoint']
+            resp['directions'].append(data)
+
+    return json.dumps(resp)
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(debug=True, host='0.0.0.0')
