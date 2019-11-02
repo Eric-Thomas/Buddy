7ac21d692afb13a5e674c3fc05939a5ac5a184d2
Eric Thomas <bluedevils027@gmail.com>
2019-11-02 13:00:00 -0400

    returning hard coded directios in /directions endpoint

--- buddy.py
+++ buddy.py
@@ -1,16 +1,22 @@
 from flask import Flask
 import requests
 import json
+import pprint
 
 key = 'D2ZCny0cmDMUDUbsavWda8qvTD9K3iRV'
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/directions')
 def get_directions():
     params = {'key': key, 'from': '1900 Summit Street Columbus Ohio', 'to': '1494 North High Street Columbus Ohio', 'routeType': 'pedestrian', 'narrativeType': 'html'}
     content = requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
     parsed_json = json.loads(content)
-    return content
+    pp = pprint.PrettyPrinter()
+    directions = {'directions': ''}
+    for leg in parsed_json['route']['legs']:
+        for maneuver in leg['maneuvers']:
+            directions['directions'] += maneuver['narrative']
+    return json.dumps(directions)
 
 
 if __name__ == '__main__':
