fd9f8637a1ab8b95486903112e19eb1e76e2b39e
Eric Thomas <bluedevils027@gmail.com>
2019-11-02 12:28:25 -0400

    mapquest request

--- buddy.py
+++ buddy.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import requests
+
+key = 'D2ZCny0cmDMUDUbsavWda8qvTD9K3iRV'
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_route():
+    params = {'key': key, 'from': '1900 Summit Street Columbus Ohio', 'to': '1494 North High Street Columbus Ohio', 'routeType': 'pedestrian', 'narrativeType': 'html'}
+    return requests.get('http://www.mapquestapi.com/directions/v2/route', params=params).content
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
