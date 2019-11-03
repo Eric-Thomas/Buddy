3d5b3cad79a0ca5d41c9730dba802d2f4ea37897
JP Dahms <jack.dahms@gmail.com>
2019-11-03 09:59:35 -0500

    fixed indentation in danger.py

--- danger.py
+++ danger.py
@@ -7,38 +7,15 @@ crime_addresses = []
 crime_coordinates = []
 crime_file = open("crimes.txt", "a")
 
-# Load crime data
-# with open('data1-107.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             print(f'Column names are {", ".join(row)}')
-#         else:
-#             crime_addresses.append(row[4])
-#         line_count += 1
-#     print(f'Processed {line_count} lines.')
-
-# for crime in crime_addresses[1300:]:
-#     try:
-#         location = geolocator.geocode(crime)
-#         coordinates = (location.latitude, location.longitude)
-#         crime_coordinates.append(coordinates)
-#         crime_file.write(str(location.latitude) + ' ' + str(location.longitude) + '\n')
-#     except Exception as e:
-#         print(e)
-
 filepath = 'crimes.txt'
 with open(filepath) as fp:
-   line = fp.readline()
-   while line:
-       line = fp.readline()
-       if line:
-        latitude = float(line.split()[0])
-        longitude = float(line.split()[1])
-        crime_coordinates.append((latitude, longitude))
-
-
+    line = fp.readline()
+    while line:
+        line = fp.readline()
+        if line:
+            latitude = float(line.split()[0])
+            longitude = float(line.split()[1])
+            crime_coordinates.append((latitude, longitude))
 
 def point_danger(coordinates):
     danger = 0
