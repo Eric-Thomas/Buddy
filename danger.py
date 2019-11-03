14a7cd602eae337d2a83783e315a841a93e76f14
Eric Thomas <bluedevils027@gmail.com>
2019-11-02 22:51:02 -0400

    Crime data baby

--- danger.py
+++ danger.py
@@ -8,33 +8,42 @@ crime_coordinates = []
 crime_file = open("crimes.txt", "a")
 
 # Load crime data
-with open('data1-107.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-        else:
-            crime_addresses.append(row[4])
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+# with open('data1-107.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#         else:
+#             crime_addresses.append(row[4])
+#         line_count += 1
+#     print(f'Processed {line_count} lines.')
 
-for crime in crime_addresses[196:]:
-    try:
-        location = geolocator.geocode(crime)
-        coordinates = (location.latitude, location.longitude)
-        crime_coordinates.append(coordinates)
-        crime_file.write(str(location.latitude) + ' ' + str(location.longitude) + '\n')
-    except Exception as e:
-        print(e)
+# for crime in crime_addresses[1250:]:
+#     try:
+#         location = geolocator.geocode(crime)
+#         coordinates = (location.latitude, location.longitude)
+#         crime_coordinates.append(coordinates)
+#         crime_file.write(str(location.latitude) + ' ' + str(location.longitude) + '\n')
+#     except Exception as e:
+#         print(e)
 
+filepath = 'crimes.txt'
+with open(filepath) as fp:
+   line = fp.readline()
+   while line:
+       line = fp.readline()
+       if line:
+        latitude = float(line.split()[0])
+        longitude = float(line.split()[1])
+        crime_coordinates.append((latitude, longitude))
 
 
 
 def point_danger(coordinates):
     danger = 0
     for crime in crime_coordinates:
-        d = (coordinates[0] - crime[0])**2 + (coordinates[1] - crime[1])**2
+        d = (coordinates['lng'] - crime[0])**2 + (coordinates['lat'] - crime[1])**2
         # 364320 feet per degree (double check this)
         # if d < (5280 * 5 / 364320)**2: # only check crimes that occured within 5 miles
         d = 1 / d # inverse square distance penalizes closer crimes
@@ -45,4 +54,5 @@ def route_danger(route):
     danger = 0
     for point in route:
         danger += point_danger(point)
+    print(danger)
     return danger
